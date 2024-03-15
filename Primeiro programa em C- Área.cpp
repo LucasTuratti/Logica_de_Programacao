00d870ea8d28f8c35cd551c597e2a181eff21e6c
LucasTuratti <lucasturatti7@gmail.com>
2024-03-15 11:57:37 -0300

    Create Primeiro programa em C- Área.cpp
    Meu Primeiro Programa em Linguagem C, feito na primeira aula de Lógica de Programação

---
+++ Primeiro programa em C- Área.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	float comprimento, largura, area;
+	
+	printf("Insira o comprimento: ");
+	scanf("%f" ,&comprimento);
+	
+	printf("Insira a largura: ");
+	scanf("%f" ,&largura);
+	
+	area = (comprimento*largura);
+	printf("Área do seu terreno: %f ",area);
+}
